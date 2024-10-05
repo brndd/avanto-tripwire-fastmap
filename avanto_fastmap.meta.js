a0d1ca926e4fee97249cdb4394b796f0dab3802f
brndd <burneddi@protonmail.com>
2024-10-06 02:36:06 +0300

    Version 0.2.2.
    No feature changes, just makes the script use meta.js to save Microsoft's bandwidth.

---
+++ avanto_fastmap.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Avanto Fastmap
+// @namespace   avanto
+// @match       https://map.avanto.tk/*
+// @require https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
+// @downloadURL https://raw.githubusercontent.com/brndd/avanto-tripwire-fastmap/refs/heads/master/avanto_fastmap.user.js
+// @updateURL https://raw.githubusercontent.com/brndd/avanto-tripwire-fastmap/refs/heads/master/avanto_fastmap.meta.js
+// @grant       none
+// @version     0.2.2
+// @author      burneddi
+// @description Adds a quick input box for adding wormholes to Tripwire using Avanto bookmark syntax.
+// ==/UserScript==
--- src/avanto_fastmap.js
+++ src/avanto_fastmap.js
@@ -4,9 +4,9 @@
 // @match       https://map.avanto.tk/*
 // @require https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
 // @downloadURL https://raw.githubusercontent.com/brndd/avanto-tripwire-fastmap/refs/heads/master/avanto_fastmap.user.js
-// @updateURL https://raw.githubusercontent.com/brndd/avanto-tripwire-fastmap/refs/heads/master/avanto_fastmap.user.js
+// @updateURL https://raw.githubusercontent.com/brndd/avanto-tripwire-fastmap/refs/heads/master/avanto_fastmap.meta.js
 // @grant       none
-// @version     0.2.1
+// @version     0.2.2
 // @author      burneddi
 // @description Adds a quick input box for adding wormholes to Tripwire using Avanto bookmark syntax.
 // ==/UserScript==
