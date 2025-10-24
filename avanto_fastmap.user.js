a4dde9c3193ef09b2be829b8eff28cef1dcec761
brndd <burneddi@protonmail.com>
2025-10-24 23:57:05 +0300

    Version 0.3.4
    Update domain to avant0.fi

--- avanto_fastmap.user.js
+++ avanto_fastmap.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        Avanto Fastmap
 // @namespace   avanto
-// @match       https://map.avanto.tk/*
+// @match       https://map.avant0.fi/*
 // @require https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
 // @downloadURL https://raw.githubusercontent.com/brndd/avanto-tripwire-fastmap/refs/heads/master/avanto_fastmap.user.js
 // @updateURL https://raw.githubusercontent.com/brndd/avanto-tripwire-fastmap/refs/heads/master/avanto_fastmap.meta.js
 // @grant       none
-// @version     0.3.3
+// @version     0.3.4
 // @author      burneddi
 // @description Adds a quick input box for adding wormholes to Tripwire using Avanto bookmark syntax.
 // ==/UserScript==
